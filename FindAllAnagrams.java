1c06494249fedd4781e39fb1d23e0fd75abfb382
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-07-14 07:51:20 +0900

    Update FindAllAnagrams.java

--- FindAllAnagrams.java
+++ FindAllAnagrams.java
@@ -1,52 +1,49 @@
-package T0710;
-
+package top50;
 import java.util.*;
 
 public class FindAllAnagrams {
 
-		  public static void main(String args[])
-		    {
-		        String txt = "BACDGABCDA";
-		        String pat = "ABCD";
-		        System.out.println(findAnagrams(txt, pat));
-		    }
-		
-		
-		 public static List<Integer> findAnagrams(String s, String p) {
-		        List<Integer> rst = new ArrayList<>();
-		        if (s == null || s.length() == 0 || s.length() < p.length()) {
-		            return rst;
-		        }
+	public static void main(String args[])
+    {
+		FindAllAnagrams a = new FindAllAnagrams();
+        String txt = "BACDGABCDA";
+        String pat = "ABCD";
+        System.out.println(a.findAnagrams(txt, pat));
+    }
 
-		        int[] map_p = new int[256];
-		        for (int i = 0; i < p.length(); i++) {
-//		            map_p[p.charAt(i) - 'A']++;
-		            map_p[p.charAt(i) ]++;
-		        }
 
-		        for (int i = 0; i < s.length() - p.length()+1; i++) {
-		            int[] map_s = new int[256];
-		            for (int j = 0; j < p.length(); j++) {
-		            	System.out.println("i: "+i+" j: "+j+" i+j: "+(i+j));
-		            	System.out.println("s.charAt(i+j): "+s.charAt(i+j));
-		                map_s[s.charAt(i+j)]++;
-		            }
-		            System.out.println();
-		            if (isMatch(map_p, map_s)) {
-		            	System.out.println("true");
-		                rst.add(i);
-		            }
-		        }
-		        return rst;
-		    }
-
-		    public static boolean isMatch(int[] arr1, int[] arr2) {
-		        for (int i = 0; i < arr1.length; i++) {
-		            if (arr1[i] != arr2[i]) {
-		                return false;
-		            }
-		        }
-		        return true;
-		    }
-
+ public List<Integer> findAnagrams(String txt, String pat) {
+   //1 
+	 List<Integer> result = new ArrayList<>();
+	 if(txt==null||txt.length()==0||pat==null||pat.length()==0||txt.length()<pat.length())
+		 return result;
+   //2
+	 int[] patArr = new int[256];//[0,0,0,0,0,0,1,1,1,1,0,0,0]
+	 for(int i=0; i<pat.length();i++) {
+		 patArr[pat.charAt(i)]++;
+	 }
+	 
+	 for(int i=0; i<txt.length()-pat.length()+1; i++) {
+		 int[] txtArr = new int[256];
+		 for(int j=0; j<pat.length(); j++) {
+			 txtArr[txt.charAt(i+j)]++; //0+, 1+
+		 }
+		 
+		 if(check(patArr, txtArr)) {
+			 result.add(i);
+		 }
+	 }
+	return result;
+  }
+ private boolean check(int[] patArr, int[] txtArr) {
+	 for(int i=0; i<patArr.length; i++) {
+		if(patArr[i] != txtArr[i]) {
+			return false;
+		}
+	 }
+	 return true;
+ }
+	 
+	 
+	 
 }
