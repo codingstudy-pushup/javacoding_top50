e0e9c0c2ca1b29378b7b68662d1fac84d631cc4d
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-07-14 09:51:11 +0900

    Update T17_GroupAnagrams.java

--- T17_GroupAnagrams.java
+++ T17_GroupAnagrams.java
@@ -15,6 +15,31 @@ public class T17_GroupAnagrams {
 	
 	 public static List<List<String>> groupAnagrams(String[] strs) {
 	        if (strs == null || strs.length == 0) return new ArrayList<List<String>>();
+	        List<List<String>> result = new ArrayList<>();
+	        Map<String, List<String>> map = new HashMap<String, List<String>>();
+	        for(String str : strs) {
+				char[] arr = new char[256];
+				for(int i=0; i<str.length(); i++) {
+					arr[str.charAt(i)]++;
+				}
+				String ns = new String(arr);
+				System.out.println("ns: "+ns);
+				
+				if(map.containsKey(ns)) {
+					map.get(ns).add(str);
+				}else {
+					ArrayList<String> al = new ArrayList<>();
+					al.add(str);
+					map.put(ns, al);
+				}
+			}
+	         result.addAll(map.values());
+	         return result;
+//	        return new ArrayList<List<String>>(map.values());
+	    }
+	
+	 public static List<List<String>> groupAnagrams(String[] strs) {
+	        if (strs == null || strs.length == 0) return new ArrayList<List<String>>();
 	        Map<String, List<String>> map = new HashMap<String, List<String>>();
 	        for (String s : strs) {
 	            char[] ca = s.toCharArray();
