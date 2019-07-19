54d158020997f8bbd16cf1dca7bb9146dede8cf7
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-07-19 17:23:17 +0900

    Update Maze1_bfs.java

--- Maze1_bfs.java
+++ Maze1_bfs.java
@@ -1,4 +1,6 @@
-package zTest;
+package zTest01;
+
+
 
 import java.util.*;
 
@@ -21,7 +23,8 @@ public class Maze1_bfs {
 		System.out.println(a.hasPath(maze, start,dest ));
 	}
 
-	int[][] dirs = {{-1,0},{0,1},{1,0},{0,-1}};
+	
+	int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
 	int m ,n;
 	
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
@@ -41,19 +44,35 @@ public class Maze1_bfs {
             	int x=p[0];
    	            int y=p[1];
                 while (x>=0 && x<m && y>=0 && y<n && maze[x][y]==0) {
+                	System.out.println("");
+                	System.out.println("11 x: "+x+" y: "+y);
                     x+=dir[0];
                     y+=dir[1];
+                    System.out.println("22x: "+x+" y: "+y);
+          
                 }
                 x-=dir[0];
                 y-=dir[1];
+                System.out.println("33x: "+x+" y: "+y);
                 if (visited[x][y]) continue;
                 visited[x][y]=true;
+                print(visited);
                 if (x==destination[0] && y==destination[1]) return true;
                 queue.offer(new int[] {x,y});
             }
+            System.out.println("");
         }
         return false;
-        
+    }
+    
+    private void print(boolean[][] visited) {
+    	if(visited==null|| visited.length==0)return;
+
+    	for(int i=0; i<m; i++) {
+    		for(int j=0; j<n; j++) {
+    			System.out.print(visited[i][j]+"\t");
+    		}
+    		System.out.println();
+    	}
     }
 }
-
