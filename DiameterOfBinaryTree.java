0991695821cee1ce44cf56e4b818d41ac88143db
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-07-26 15:31:22 +0900

    Update DiameterOfBinaryTree.java

--- DiameterOfBinaryTree.java
+++ DiameterOfBinaryTree.java
@@ -1,42 +1,43 @@
-package zTest;
+package zTest01;
 
-
-
-class TreeNode{
+class TreeNode {
 	int val;
 	TreeNode left, right;
-	TreeNode(int x){
+
+	TreeNode(int x) {
 		this.val = x;
 	}
 }
 
+public class MaxDepth {
 
-public class DiameterOfBinaryTree {
 	public static void main(String[] args) {
-		DiameterOfBinaryTree a = new DiameterOfBinaryTree();
-		TreeNode node = new TreeNode(1);
-		node.left = new TreeNode(2);
-		node.right = new TreeNode(3);
-		node.left.left = new TreeNode(4);
-		node.left.right = new TreeNode(5);
-		
-		System.out.println("val: "+a.solve(node));
+		MaxDepth a = new MaxDepth();
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		System.out.println(a.solve(root));
 	}
-	
-	int max =0;
+
+	int max = 0;
+
 	public int solve(TreeNode root) {
-        maxDepth(root);
-        return max;
-    }
-    
-    private int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        
-        max = Math.max(max, left + right);
-        
-        return Math.max(left, right) + 1;
-    }
+		maxDepth(root);
+		return max;
+	}
+
+	public int maxDepth(TreeNode root) {
+		if (root == null)
+			return 0;
+		int left = maxDepth(root.left);
+		int right = maxDepth(root.right);
+
+		max = Math.max(left + right, max);
+
+		return Math.max(left, right) + 1;
+
+	}
+
 }
