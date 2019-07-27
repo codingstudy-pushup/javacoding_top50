4d4c373c7445f0daf626d9ed341de9cb7384942a
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-07-28 06:35:53 +0900

    Update WordLadder.java

--- WordLadder.java
+++ WordLadder.java
@@ -1,4 +1,97 @@
-package top50;
+package top50
+
+import java.util.Arrays;
+import java.util.*;
+
+public class WordLadder {
+
+	public static void main(String[] args) {
+		String[] words = {"hot","dot","lot","log","cog"};
+//		String[] words = {"hot","dot","dog","lot","log"};
+		List<String> wordList = Arrays.asList(words);
+//		Set<String> dict = new HashSet<>(wordList);
+		WordLadder a = new WordLadder();
+		System.out.println(a.ladderLength_neighbor("hit","cog", wordList));
+	}
+	
+	 public int ladderLength_neighbor(String beginWord, String endWord, List<String> wordList) {
+		   if(wordList==null|| !wordList.contains(endWord))
+			   return 0;
+		 
+	        Queue<String> queue = new LinkedList<>();
+	        Set<String> dict = new HashSet<>(wordList);
+	        queue.offer(beginWord);
+//	        dict.add(endWord);
+//	        dict.remove(beginWord);
+	        int level = 1;
+	        while(!queue.isEmpty()){
+	            int size = queue.size();
+	            System.out.println("================size "+size);
+	            for(int i=0;i<size;i++){
+	                String str = queue.poll();
+//	                System.out.println("=======str "+str);
+	                if(str.equals(endWord))return level;
+	                for(String neighbor : neighbors(str,wordList)){
+	                	System.out.println("neighbor "+neighbor);
+	                    queue.offer(neighbor);
+	                }
+	            }
+	            level++;
+	        }
+	        return 0;
+	    }
+	    
+	    public List<String> neighbors(String s, List<String> wordList){
+	     	 System.out.println("&&&&&&&&&&&&&&&&&&neighbors "+s);
+	        List<String> res = new LinkedList<>();
+	        Set<String> dict = new HashSet<>(wordList);
+	        for(int i=0;i<s.length();i++){
+	            char [] chars = s.toCharArray();
+	            for(char ch = 'a'; ch <= 'z'; ch++){
+	                chars[i] = ch;
+	                String word = new String(chars);
+//	                System.out.println("word "+word);
+	                if(dict.remove(word)){
+	               	 System.out.println("&&&&&&&&&&&&&&&&&&word "+word);
+	                	 System.out.println("&&&&&&&&&&&&&&&&&&dict "+dict);
+	                
+	                    res.add(word);
+	                }
+	            }
+	        }
+	        return res;
+	    }
+	    
+	
+
+	 
+	    
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 import java.util.Arrays;
 import java.util.*;
