4818e2a2aa0175565096785f3ed16ddcec39aea0
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-07-28 21:53:35 +0900

    Create ClimbingStairs.java

---
+++ ClimbingStairs.java
@@ -0,0 +1,28 @@
+package top50;
+
+
+
+public class ClimbingStairs {
+
+	public static void main(String[] args) {
+		int n =5;
+		System.out.println(climbStairs(n));
+	}
+	public static int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+          dp[i] = dp[i-1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
+
