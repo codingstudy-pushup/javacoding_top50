46f1ee5233e5e3c071147a1431e785e73e12a4f6
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-07-29 20:34:29 +0900

    Update MergeKSortedList.java

--- MergeKSortedList.java
+++ MergeKSortedList.java
@@ -4,58 +4,63 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class MergeKSortedList {
-	
-	   public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		ListNode l1 = new ListNode(1);
 		l1.next = new ListNode(4);
 		l1.next.next = new ListNode(5);
-		
+
 		ListNode l2 = new ListNode(1);
 		l2.next = new ListNode(3);
 		l2.next.next = new ListNode(4);
-		
+
 		ListNode l3 = new ListNode(2);
 		l3.next = new ListNode(6);
-		
+
 		ListNode[] list = new ListNode[3];
 		list[0] = l1;
 		list[1] = l2;
 		list[2] = l3;
-		ListNode result = mergeKLists(list);
-	   }
-	   
-	   public static ListNode mergeKLists(ListNode[] lists) {
-			//1
-//			PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator <ListNode>() {
-//				public int compare(ListNode l1, ListNode l2) {
-//					return l1.val -l2.val;
-//				}
-//			});
-		   //2
-		   PriorityQueue <ListNode> queue = new PriorityQueue<ListNode>(Comp);
-			
-			ListNode head = new ListNode(0);
-			ListNode p = head;
-			
-			for(ListNode list : lists) {
-				if(list != null)
-					queue.offer(list);
-			}
-			
-			while(!queue.isEmpty()) {
-				ListNode n = queue.poll();
-				System.out.print(n.val+"\t");
-				p.next = n;
-				p = p.next;
-				if(n.next != null)
-					queue.offer(n.next);
-			}
-			return head.next;
+		MergeKSortedList a = new MergeKSortedList();
+		ListNode result = a.mergeKLists(list);
+		System.out.println("====");
+		while(result !=null) {
+			System.out.println(result.val);
+			result= result.next;
 		}
-	   
-	   static Comparator<ListNode> Comp = new Comparator<ListNode>(){
-	       public int compare(ListNode a, ListNode b){
-	           return a.val-b.val;
-	       }
-	    };
+	}
+
+	public ListNode mergeKLists(ListNode[] lists) {
+		// 1
+		PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(Comp);
+		ListNode newHead = new ListNode(0);
+		ListNode p = newHead;
+		for (ListNode node : lists) {
+			if (node != null)
+				queue.offer(node);
+		}
+
+		// 2
+		while (!queue.isEmpty()) {
+			ListNode node = queue.poll();
+			System.out.print(""+node.val+"\t");
+			p.next = node;
+			p = p.next;
+			if (node.next != null)
+				queue.offer(node.next);
+		}
+		return newHead.next;
+
+	}
+
+	Comparator<ListNode> Comp = new Comparator<ListNode>() {
+
+		@Override
+		public int compare(ListNode a, ListNode b) {
+			// TODO Auto-generated method stub
+			return a.val - b.val;
+		}
+
+	};
+
 }
