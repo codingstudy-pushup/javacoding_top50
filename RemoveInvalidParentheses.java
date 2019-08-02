f2f57554b2e4dd23130d0db8352b4934b0aa6627
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-08-02 10:36:10 +0900

    Update RemoveInvalidParentheses.java

--- RemoveInvalidParentheses.java
+++ RemoveInvalidParentheses.java
@@ -2,70 +2,61 @@ package zTest01;
 
 import java.util.*;
 
-public class RemoveInvalidParentheses {
+public class RemoveInvalidParentheses01 {
 
-	
 	public static void main(String[] args) {
-//		String s = "()())()";
 		String s = "(a)())()";
-		RemoveInvalidParentheses a = new RemoveInvalidParentheses();
-		System.out.println(a.removeInvalidParentheses(s));
-		
+		RemoveInvalidParentheses01 a = new RemoveInvalidParentheses01();
+		System.out.println(a.solve(s));
 	}
-	 public List<String> removeInvalidParentheses(String s) {
-	        List<String> res = new ArrayList<>();
-	        if (s == null){
-	            return res;
-	        }
-	        Queue<String> queue = new LinkedList<>();
-	        Set<String> visited = new HashSet<>();
-	        queue.offer(s);
-	        visited.add(s);
-	        boolean found = false;
-	        while(!queue.isEmpty()){
-	            int size = queue.size();
-	            System.out.println("size "+size);
-	            for (int j = 0; j < size; j++){
-	                String curr = queue.poll();
-	                if (isValid(curr)){
-	                    res.add(curr);
-	                    found = true;
-	                }
-	                if (found) continue;
-//	                if (!found){
-	                    for (int i = 0; i < curr.length(); i++){
-//	                        char c = curr.charAt(i);
-//	                        if (Character.isLetter(c)){
-//	                            continue;
-//	                        }
-	                        if (curr.charAt(i) != '(' && curr.charAt(i) != ')') continue;
-	                        System.out.println("i:"+i+""+curr.substring(0, i)+" : "+curr.substring(i+1));
-	                        String str = curr.substring(0, i) + curr.substring(i+1);
-	                        System.out.println("str "+str);
-	                        if (!visited.contains(str)){
-	                            queue.offer(str);
-	                            visited.add(str);
-	                        }
-	                    }
-//	                }
-	            }   
-	            System.out.println();
-	        }
-	        return res;
+	public List<String> solve(String s){
+		List<String> result = new ArrayList<>();
+		if(s==null) 
+			return result;
+		Queue<String> queue = new LinkedList<>();
+		Set<String> visited = new HashSet<>();
+		queue.offer(s);
+		visited.add(s);
+		boolean found =false;
+		
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			for(int i=0; i<size; i++) {
+				String str = queue.poll();
+				if(isValid(str)) {
+					result.add(str);
+					found = true;
+					System.out.println("str: "+str+" found "+found);
+				}
+				System.out.println("found "+found);
+				if(found) continue;
+				for(int j=0; j<str.length(); j++) {
+					if(str.charAt(j)!= '(' && str.charAt(j) !=')') continue;
+					System.out.println(str.substring(0,j)+":"+str.substring(j+1));
+					String newStr = str.substring(0,j)+str.substring(j+1);
+					if(!visited.contains(newStr)) {
+//						System.out.println("newStr: "+newStr);
+						queue.offer(newStr);
+						visited.add(newStr);
+					}
+				}
+				System.out.println("======");
+			}
+		}
+		return result;
+	}
+	private boolean isValid(String str) {
+		int count=0;
+		
+	    for(char c : str.toCharArray()) {
+	    	if(c=='(') {
+	    		count++;
+	    	}else if(c==')') {
+	    		count--;
+	    		if(count<0) return false;
+	    	}
 	    }
-	    private boolean isValid(String str){
-	        int count = 0;
-	        for (char c : str.toCharArray()){
-	            if (c == '('){
-	                count++;
-	            }else if (c == ')'){
-	                count--;
-	                if (count < 0){
-	                    return false;
-	                }
-	            }
-	        }
-	        return count == 0;
-	    }
+	    return count==0;
+	}
 	
 }
