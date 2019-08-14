f22f3029b81ffbc7085fa13a809978e44b4b2bb8
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-08-15 08:13:24 +0900

    Update SpiralMatrix.java

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -5,58 +5,52 @@ import java.util.*;
 public class SpiralMatrix {
 
 	public static void main(String[] args) {
-		int[][] matrix =
-			{
-					{ 1, 2, 3 },
-					 { 4, 5, 6 },
-					 { 7, 8, 9 }
-			};
-		System.out.println(spiralOrder(matrix));
+		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		System.out.println(solve(matrix));
 	}
-	
-	  public static List<Integer> spiralOrder(int[][] matrix) {
-	        
-	        List<Integer> res = new ArrayList<Integer>();
-	        
-	        if (matrix.length == 0) {
-	            return res;
-	        }
-	        
-	        int rowBegin = 0;
-	        int rowEnd = matrix.length-1;
-	        int colBegin = 0;
-	        int colEnd = matrix[0].length - 1;
-	        
-	        while (rowBegin <= rowEnd && colBegin <= colEnd) {
-	            // Traverse Right
-	            for (int j = colBegin; j <= colEnd; j ++) {
-	                res.add(matrix[rowBegin][j]);
-	            }
-	            rowBegin++;
-	            
-	            // Traverse Down
-	            for (int j = rowBegin; j <= rowEnd; j ++) {
-	                res.add(matrix[j][colEnd]);
-	            }
-	            colEnd--;
-	            
-	            if (rowBegin <= rowEnd) {
-	                // Traverse Left
-	                for (int j = colEnd; j >= colBegin; j --) {
-	                    res.add(matrix[rowEnd][j]);
-	                }
-	            }
-	            rowEnd--;
-	            
-	            if (colBegin <= colEnd) {
-	                // Traver Up
-	                for (int j = rowEnd; j >= rowBegin; j --) {
-	                    res.add(matrix[j][colBegin]);
-	                }
-	            }
-	            colBegin ++;
-	        }
-	        
-	        return res;
-	    }
+
+	public static List<Integer> solve(int[][] matrix) {
+		// 1
+		List<Integer> result = new ArrayList<>();
+		if (matrix == null || matrix.length == 0)
+			return result;
+		int rowStart = 0;
+		int rowEnd = matrix.length - 1;
+
+		int colStart = 0;
+		int colEnd = matrix[0].length - 1;
+
+		//
+		while (rowStart <= rowEnd && colStart <= colEnd) {
+			// right
+			for (int i = colStart; i <= colEnd; i++) {
+				result.add(matrix[rowStart][i]);
+			}
+			rowStart++;
+			// down
+			for (int i = rowStart; i <= rowEnd; i++) {
+				result.add(matrix[i][colEnd]);
+			}
+			colEnd--;
+
+			// left 3->0
+			if (rowStart <= rowEnd) {
+				for (int i = colEnd; i >= colStart; i--) {
+					result.add(matrix[rowEnd][i]);
+				}
+			}
+			rowEnd--;
+			// up 2->1
+			if (colStart <= colEnd) {
+				if (rowStart <= rowEnd) {
+					for (int i = rowEnd; i >= rowStart; i--) {
+						result.add(matrix[i][colStart]);
+					}
+				}
+			}
+			colStart++;
+		}
+		return result;
+
+	}
 }
