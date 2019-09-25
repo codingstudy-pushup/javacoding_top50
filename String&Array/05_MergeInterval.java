6cbc2aabb3d04368720f4dc11b761655638e8467
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2019-09-26 06:58:23 +0900

    Update 05_MergeInterval.java

--- String&Array/05_MergeInterval.java
+++ String&Array/05_MergeInterval.java
@@ -2,51 +2,95 @@ package Top50_test;
 
 import java.util.*;
 
-//1
 class Interval {
 	int start;
 	int end;
 
+	Interval() {
+		start = 0;
+		end = 0;
+	}
+
 	Interval(int s, int e) {
-		this.start = s;
-		this.end = e;
+		start = s;
+		end = e;
 	}
 }
 
-public class T04_MergeInterval {
-	// 2
+public class T04_MergeInterval2 {
+
 	public static void main(String[] args) {
-		Interval in = new Interval(1, 3);
-		Interval in3 = new Interval(2, 6);
-		Interval in2 = new Interval(8, 10);
+
+//		int[][] nums = {{1,3},{2,6},{8,10},{15,18}};
+		Interval in1 = new Interval(1, 3);
+		Interval in2 = new Interval(2, 6);
+		Interval in3 = new Interval(8, 10);
 		Interval in4 = new Interval(15, 18);
-		T04_MergeInterval a = new T04_MergeInterval();
-		List<Interval> list = new ArrayList<>();
-		list.add(in);
-		list.add(in2);
-		list.add(in3);
-		list.add(in4);
-		a.solve(list);
+
+		List<Interval> intervals = new ArrayList<>();
+		intervals.add(in1);
+		intervals.add(in2);
+		intervals.add(in3);
+		intervals.add(in4);
+		T04_MergeInterval2 a = new T04_MergeInterval2();
+		List<Interval> list = a.merge(intervals);
+		a.print(list);
 	}
 
-	public List<Interval> solve(List<Interval> list) {
-		List<Interval> result = new ArrayList<>();
-		if (list == null || list.size() == 0)
-			return result;
-		print(list);
+	public List<Interval> merge(List<Interval> intervals) {
+		if (intervals.isEmpty())
+			return intervals;
 
-		Collections.sort(list, comp2);
-		System.out.println("====after=====");
-		print(list);
+		Collections.sort(intervals, comp2);
+//		Collections.sort(intervals,(a,b) -> a.start-b.start);
 
-		// 4
-		Interval in = list.get(0);
+		List<Interval> ans = new LinkedList<Interval>();
+		Interval hold = intervals.get(0);
+		for (int i = 1; i < intervals.size(); i++) {
+			Interval current = intervals.get(i);
+			if (hold.end >= current.start) {
+				System.out.println("hold.end: " + hold.end);
+				hold.end = Math.max(current.end, hold.end);
+			} else {
+				System.out.println("22hold.end: " + hold.end);
+				ans.add(hold);
+				hold = current;
+			}
+		}
 
-		return list;
+		if (!ans.contains(hold)) {
+			System.out.println("333hold.end: " + hold.end);
+			ans.add(hold);
+		}
+
+		return ans;
 	}
 
-	Comparator<Interval> comp = new Comparator<Interval>() {
+	// 1
+	Comparator comp = new Comparator<Interval>() {
+		public int compare(Interval a, Interval b) {
+			// TODO Auto-generated method stub
+			return a.start - b.start;
+		}
+	};
+// 2
+//	 리턴값이 int 양수: 현재 인스턴스가 비교대상인 인스턴스보다 크다 
+//            음수:현재 인스턴스가비교대상인 인스턴스보다 작다
+//               0 :값이 동일
+//
+//	public int compare(Interval o1, Interval o2) {
+//		if (o1.start > o2.start) {
+//			return 1;
+//		} else if (o1.start < o2.start) {
+//			return -1;
+//		} else {
+//			return 0;
+//		}
+//	}
+
+	Comparator<Interval> comp2 = new Comparator<Interval>() {
 		@Override
+
 		public int compare(Interval o1, Interval o2) {
 			if (o1.start > o2.start) {
 				return 1;
@@ -58,23 +102,10 @@ public class T04_MergeInterval {
 		}
 	};
 
-	Comparator<Interval> comp2 = new Comparator<Interval>() {
-
-		@Override
-		public int compare(Interval o1, Interval o2) {
-			// TODO Auto-generated method stub
-			return o1.start - o2.start;
-		}
-
-	};
-
-	public void print(List<Interval> list) {
-		if (list != null) {
-			for (Interval in : list) {
-				if (in != null) {
-					System.out.println(in.start + " " + in.end);
-				}
-			}
+	void print(List<Interval> list) {
+		for (int i = 0; i < list.size(); i++) {
+			Interval in = list.get(i);
+			System.out.println(in.start + " " + in.end);
 		}
 	}
 }
