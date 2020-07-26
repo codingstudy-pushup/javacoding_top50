79921d4605d4c01b89331b448e54bed8760ab1ca
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2020-07-26 23:28:40 +0900

    Update 04_Lis.java

--- Dynamic/04_Lis.java
+++ Dynamic/04_Lis.java
@@ -9,7 +9,9 @@ public class Lis {
 		
 		Lis a = new Lis();
 		int[] nums = {1,2,3, 2, 5, 2, 6, 10, 4, 12}; 
-		System.out.println(a.solve(nums));
+//		int[] nums = {10,9,2,5,3,7,101,18};
+
+		System.out.println("value "+ a.solve(nums));
 	}
     public int solve(int[] nums) {
         if(nums == null || nums.length==0)
@@ -19,15 +21,16 @@ public class Lis {
         int result = 1;
         for(int i=1; i<nums.length; i++){
             for(int j=0; j<i; j++){
-//            	System.out.println("nums["+i+"] "+nums[i]+" nums["+j+"]"+nums[j]);
+            	System.out.println("nums["+i+"] "+nums[i]+" nums["+j+"]"+nums[j]);
                 if(nums[i]>nums[j]){
                 	
                     dp[i]=Math.max(dp[j]+1,dp[i]);
-//                    System.out.println("dp["+i+"] "+dp[i]+" dp["+j+"]"+dp[j]);
+                    System.out.println("dp["+i+"] "+dp[i]+" dp["+j+"]"+dp[j]);
                 }
             }
             System.out.println();
             result=Math.max(result, dp[i]);
+            System.out.println("result: "+result);
         }
         return result;
         
