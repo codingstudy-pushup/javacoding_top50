fa6b0d5f8fda6328e98e174f552857365d984aac
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2020-10-04 08:45:12 +0900

    Update 10_PlusOne.java

--- String&Array/10_PlusOne.java
+++ String&Array/10_PlusOne.java
@@ -12,7 +12,8 @@ public class PlusOne {
 			System.out.println("val: " + i);
 	}
 
-	public static int[] plusOne(int[] digits) {
+       public static int[] plusOne(int[] digits) {
+
 		for (int i = digits.length - 1; i >= 0; i--) {
 			if (digits[i] != 9) {
 				digits[i]++;
@@ -20,12 +21,13 @@ public class PlusOne {
 			} else {
 				digits[i] = 0;
 			}
+		}
 
-			if (i == 0) {
-				int[] res = new int[digits.length + 1];
-				res[0] = 1;
-				return res;
-			}
+		// 0,0,0 ->1,0,0,0
+		if (digits[0] == 0) {
+			int[] result = new int[digits.length + 1];
+			result[0] = 1;
+			return result;
 		}
 
 		return digits;
