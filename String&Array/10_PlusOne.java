8f604ee6bc9dd21406925d9209703e141952d079
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2020-12-02 00:25:22 +0900

    Update 10_PlusOne.java

--- String&Array/10_PlusOne.java
+++ String&Array/10_PlusOne.java
@@ -1,36 +1,44 @@
-package top50;
+package Top50_test;
 
-public class PlusOne {
+public class T18_PlusOne {
 
+	
 	public static void main(String[] args) {
 //		int[] digits = {1,2,3};
 //		int[] digits = {9,9,9};
-		int[] digits = { 0, 0, 1 };
-
-		int[] result = plusOne(digits);
+//		int[] digits = {9,8,9};
+//		int[] digits = {8,9,9};
+//		int[] digits = {0,0,1};
+		int[] digits = {0,0};
+//		int[] digits = {9,0,0,9};
+		
+		int[] result = plusOne2(digits);
 		for (int i : result)
 			System.out.println("val: " + i);
 	}
-
-       public static int[] plusOne(int[] digits) {
-
-		for (int i = digits.length - 1; i >= 0; i--) {
-			if (digits[i] != 9) {
-				digits[i]++;
-				break;
-			} else {
-				digits[i] = 0;
-			}
-		}
-
-		// 0,0,0 ->1,0,0,0
-		if (digits[0] == 0) {
-			int[] result = new int[digits.length + 1];
-			result[0] = 1;
-			return result;
-		}
-
-		return digits;
-	}
+	
+	  public static int[] plusOne2(int[] digits) 
+	    {
+	        int carry = 1;
+	        int index = digits.length - 1;
+	        while (index >= 0 && carry > 0)
+	        {
+	            digits[index] = (digits[index] + carry) % 10;
+	            
+	            if(digits[index]==0) {
+	            	carry =1;
+	            }else {
+	            	carry =0;
+	            }
+	            index--;
+	        }
+	        
+	        if (carry > 0)
+	        {
+	            digits = new int[digits.length + 1];
+	            digits[0] = 1;
+	        }
+	        
+	        return digits;
+	    }
 }
-
