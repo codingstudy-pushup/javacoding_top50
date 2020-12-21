809e704c76a3b8a574cb11daf0faffa9cf175603
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2020-12-21 16:18:01 +0900

    Update 05_MergeInterval.java

--- String&Array/05_MergeInterval.java
+++ String&Array/05_MergeInterval.java
@@ -1,27 +1,23 @@
-package Top50_test;
+package Sort;
 
 import java.util.*;
 
+//1
 class Interval {
 	int start;
 	int end;
 
-	Interval() {
-		start = 0;
-		end = 0;
-	}
-
 	Interval(int s, int e) {
-		start = s;
-		end = e;
+		this.start = s;
+		this.end = e;
 	}
 }
 
-public class T04_MergeInterval2 {
+public class SortTest02_Com {
 
 	public static void main(String[] args) {
 
-//		int[][] nums = {{1,3},{2,6},{8,10},{15,18}};
+//			int[][] nums = {{1,3},{2,6},{8,10},{15,18}};
 		Interval in1 = new Interval(1, 3);
 		Interval in2 = new Interval(2, 6);
 		Interval in3 = new Interval(8, 10);
@@ -32,7 +28,7 @@ public class T04_MergeInterval2 {
 		intervals.add(in2);
 		intervals.add(in3);
 		intervals.add(in4);
-		T04_MergeInterval2 a = new T04_MergeInterval2();
+		SortTest02_Com a = new SortTest02_Com();
 		List<Interval> list = a.merge(intervals);
 		a.print(list);
 	}
@@ -40,54 +36,42 @@ public class T04_MergeInterval2 {
 	public List<Interval> merge(List<Interval> intervals) {
 		if (intervals.isEmpty())
 			return intervals;
-
-		Collections.sort(intervals, comp2);
+         
+	    //type1
 //		Collections.sort(intervals,(a,b) -> a.start-b.start);
+		//type2
+		Collections.sort(intervals, comp2);
 
-		List<Interval> ans = new LinkedList<Interval>();
-		Interval hold = intervals.get(0);
+
+		List<Interval> result = new ArrayList<Interval>();
+		Interval before = intervals.get(0);
 		for (int i = 1; i < intervals.size(); i++) {
 			Interval current = intervals.get(i);
-			if (hold.end >= current.start) {
-				System.out.println("hold.end: " + hold.end);
-				hold.end = Math.max(current.end, hold.end);
+			if (before.end >= current.start) {
+				before.end = Math.max(current.end, before.end);
 			} else {
-				System.out.println("22hold.end: " + hold.end);
-				ans.add(hold);
-				hold = current;
+				result.add(before);
+				before = current;
 			}
 		}
 
-		if (!ans.contains(hold)) {
-			System.out.println("333hold.end: " + hold.end);
-			ans.add(hold);
+		if (!result.contains(before)) {
+			System.out.println("before.end: " + before.end);
+			result.add(before);
 		}
 
-		return ans;
+		return result;
 	}
 
-	// 1
+	//type2
 	Comparator comp = new Comparator<Interval>() {
 		public int compare(Interval a, Interval b) {
 			// TODO Auto-generated method stub
 			return a.start - b.start;
 		}
 	};
-// 2
-//	 리턴값이 int 양수: 현재 인스턴스가 비교대상인 인스턴스보다 크다 
-//            음수:현재 인스턴스가비교대상인 인스턴스보다 작다
-//               0 :값이 동일
-//
-//	public int compare(Interval o1, Interval o2) {
-//		if (o1.start > o2.start) {
-//			return 1;
-//		} else if (o1.start < o2.start) {
-//			return -1;
-//		} else {
-//			return 0;
-//		}
-//	}
 
+	//type3
 	Comparator<Interval> comp2 = new Comparator<Interval>() {
 		@Override
 
@@ -108,4 +92,5 @@ public class T04_MergeInterval2 {
 			System.out.println(in.start + " " + in.end);
 		}
 	}
+
 }
