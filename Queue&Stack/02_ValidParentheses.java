a7201704f9b15cd4c2aea7a243190e965f7bc1b6
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2020-12-24 19:29:09 +0900

    Update 02_ValidParentheses.java

--- Queue&Stack/02_ValidParentheses.java
+++ Queue&Stack/02_ValidParentheses.java
@@ -5,34 +5,29 @@ import java.util.*;
 public class ValidParentheses {
 
 	public static void main(String[] args) {
-		String exp="{()}";
-		System.out.println(isValid(exp));
+		//String str= "()[]{}"  ;//true;
+		//String str= "(}"  ;//false;
+		String exp = "([}}])";
+		System.out.println(solve(exp));
 	}
 	
-	public static boolean isValid(String s) {
-
-	    if(s.length()%2 != 0) return false;
-	    Stack<Character> stack = new Stack<Character>();
-
-	    for(int i=0; i<s.length(); i++){
-	        switch(s.charAt(i)){
-	            case ')':
-	                if(!stack.empty()&&stack.peek()=='(') stack.pop();
-	                break;
-	            case ']':
-	                if(!stack.empty()&&stack.peek()=='[') stack.pop();
-	                break;
-	            case '}':
-	                if(!stack.empty()&&stack.peek()=='{') stack.pop();
-	                break;
-	            default:
-	                stack.push(s.charAt(i));
-	                break;
-	        }
-	        
-	    }
-	    
-	    return stack.empty();
+	
+	public static boolean solve(String str) {
+		Stack<Character> stack = new Stack<>();
+		for(char c: str.toCharArray()) {
+			System.out.println("c: "+c);
+			if(c=='(') {
+				stack.push(')');
+			}else if(c=='[') {
+				stack.push(']');
+			}else if(c=='{') {
+				stack.push('}');
+			}else if(stack.isEmpty()|| stack.pop() != c) {
+				return false;
+			}
+			
+		}
+		return stack.isEmpty();
 	}
 	
 	public static boolean isValid_map(String str) {
