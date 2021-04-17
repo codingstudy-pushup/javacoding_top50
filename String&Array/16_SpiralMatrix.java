637f5f2504246d4ca5bd60131905c262ac6cdd18
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-04-18 08:37:02 +0900

    Update 16_SpiralMatrix.java

--- String&Array/16_SpiralMatrix.java
+++ String&Array/16_SpiralMatrix.java
@@ -1,15 +1,26 @@
-package top50;
+package Top50_test;
 
 import java.util.*;
+import java.util.List;
 
 public class SpiralMatrix {
 
 	public static void main(String[] args) {
-		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-		System.out.println(solve(matrix));
-	}
+//		int[][] nums = {
+//				{1, 2, 3 },
+//		                {4, 5, 6 },
+//		                {7, 8, 9 }
+//				};
+		int[][] nums = { { 1, 2,  3,  4 },
+				 { 5, 6,  7,  8 }, 
+				 { 9, 10, 11, 12 } };
+		System.out.println(spiralOrder(nums));
+		System.out.println(spiralOrder2(nums));
 
-	public static List<Integer> solve(int[][] matrix) {
+	}
+	
+
+	public static List<Integer> spiralOrder(int[][] matrix) {
 		// 1
 		List<Integer> result = new ArrayList<>();
 		if (matrix == null || matrix.length == 0)
@@ -34,23 +45,62 @@ public class SpiralMatrix {
 			colEnd--;
 
 			// left 3->0
+			System.out.println("rowStart: "+rowStart+" rowEnd: "+rowEnd);
+		
 			if (rowStart <= rowEnd) {
 				for (int i = colEnd; i >= colStart; i--) {
+					System.out.println("matrix["+rowEnd+"]["+i+"] "+matrix[rowEnd][i]);
 					result.add(matrix[rowEnd][i]);
 				}
 			}
 			rowEnd--;
 			// up 2->1
 			if (colStart <= colEnd) {
-				if (rowStart <= rowEnd) {
 					for (int i = rowEnd; i >= rowStart; i--) {
 						result.add(matrix[i][colStart]);
 					}
-				}
 			}
 			colStart++;
 		}
 		return result;
-
 	}
+	
+	// switch case 문 적용
+	public static List<Integer> spiralOrder2(int[][] matrix) {
+		List<Integer> result = new ArrayList<>();
+	    if(matrix == null || matrix.length == 0) return result;
+	    
+	    int rowStart = 0, rowEnd = matrix.length-1;
+	    int colStart = 0, colEnd = matrix[0].length - 1;
+	    int dir = 0;
+	    
+	    while(rowStart <= rowEnd && colStart <= colEnd) {
+	        switch(dir) {
+	            case 0: //Right
+	                for(int col = colStart; col <= colEnd; col++)
+	                    result.add(matrix[rowStart][col]);
+	                rowStart++;
+	                break;
+	            case 1: //Down
+	                for(int row = rowStart; row <=rowEnd; row++) 
+	                    result.add(matrix[row][colEnd])    ;
+	                colEnd--;
+	                break;
+	            case 2://Left
+	                for(int col = colEnd; col >= colStart; col --) 
+	                    result.add(matrix[rowEnd][col]);
+	                rowEnd--;
+	                break;
+	            case 3://Up
+	                for(int row = rowEnd; row >= rowStart; row--)
+	                    result.add(matrix[row][colStart]);
+	                colStart++;  
+	                break;
+	        }                                   
+	        dir = (dir+1)%4;                                                            
+	    }
+	    
+	    return result;
+	}
+
 }
