a9e17120f4d7eee7f73b7fd0a44690e7d7a66018
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-06-07 13:15:37 +0900

    Update 01_NumberOfIsland_dfs.java

--- Graph(dfs&bfs)/01_NumberOfIsland_dfs.java
+++ Graph(dfs&bfs)/01_NumberOfIsland_dfs.java
@@ -1,46 +1,66 @@
-package zTest;
+package graph;
 
 public class NumberOfIsland_dfs {
+
 	public static void main(String[] args) {
-		char[][] grid= {
-						{'1','1','0','0','0'},
-						{'1','1','0','0','0'},
-						{'0','0','1','0','0'},
-						{'0','0','0','1','1'}
-					   };
-		
+		char[][] grid = { 
+				{ '1', '1', '0', '0', '0' }, 
+				{ '1', '1', '0', '0', '0' }, 
+				{ '1', '1', '0', '0', '0' },
+				{ '0', '0', '0', '1', '1' } };
+
+//		char[][] grid = {
+//				{ '1', '0' },
+//				{ '1', '1'}};
+
 		NumberOfIsland_dfs a = new NumberOfIsland_dfs();
 		System.out.println(a.numsIslands(grid));
 	}
-	int[][] dirs= {{0,1},{0,-1},{1,0},{-1,0}};
+
+	int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 	int m, n;
-	int numsIslands(char[][] grid) {
-		if(grid == null|| grid.length==0 || grid[0].length==0) return 0;
+	int dfsCount = 0;
+
+	public int numsIslands(char[][] grid) {
+		if (grid == null || grid.length == 0 || grid[0].length == 0)
+			return 0;
 		m = grid.length;
 		n = grid[0].length;
-		int count =0;
-		for(int i=0; i<m; i++) {
-			for(int j=0; j<n; j++) {
-				if(grid[i][j]=='1') {
+		int count = 0;
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] == '1') {
+					System.out.println("i " + i + " j " + j);
 					count++;
-					System.out.println("i: "+i+" y: "+j);
-					merge(grid,i,j);
+					dfs(grid, i, j);
 				}
 			}
 		}
 		return count;
 	}
-	void merge(char[][] grid, int i, int j) {
-		System.out.println("==merge=====i: "+i+" y: "+j);
-		if(i<0 || i>=m || j<0||j>=n || grid[i][j]!='1') return ;
-		grid[i][j]='X';
-//		for(int[] dir: dirs) {
-//			merge(grid, i+dir[0], j+dir[1]);
-//		}
-		
-		merge(grid, i-1, j);
-		merge(grid, i+1, j);
-		merge(grid, i, j+1);
-		merge(grid, i, j-1);
+
+	public void dfs(char[][] grid, int i, int j) {
+		dfsCount++;
+		System.out.println("===i " + i + " j " + j + " dfsCount " + dfsCount);
+
+		// 1.error
+//		if(i<0 || j<0 || i >= m || j >= n || grid[i][j]!='1') 
+//			return;
+		if (i >= 0 && j >= 0 && i < m && j < n && grid[i][j] == '1') {
+
+			grid[i][j] = 'X';
+			System.out.println("grid[" + i + "][" + j + "] " + grid[i][j]);
+			for (int[] dir : dirs) {
+				dfs(grid, i + dir[0], j + dir[1]);
+			}
+			System.out.println("====end====" + " dfsCount " + dfsCount);
+		}
+
+//		dfs(grid, i-1,j);
+//		dfs(grid, i+1,j);
+//		dfs(grid, i,j-1);
+//		dfs(grid, i,j+1);
 	}
+
 }
