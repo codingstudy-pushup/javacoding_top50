e072d9fb2cf46106cef5c1d12abee9386cb68b6a
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-06-07 14:16:40 +0900

    Update 01_NumberOfIsland_dfs.java

--- Graph(dfs&bfs)/01_NumberOfIsland_dfs.java
+++ Graph(dfs&bfs)/01_NumberOfIsland_dfs.java
@@ -14,14 +14,14 @@ public class NumberOfIsland_dfs {
 //				{ '1', '1'}};
 
 		NumberOfIsland_dfs a = new NumberOfIsland_dfs();
-		System.out.println(a.numsIslands(grid));
+		System.out.println(a.solve(grid));
 	}
 
 	int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 	int m, n;
 	int dfsCount = 0;
 
-	public int numsIslands(char[][] grid) {
+	public int solve(char[][] grid) {
 		if (grid == null || grid.length == 0 || grid[0].length == 0)
 			return 0;
 		m = grid.length;
@@ -31,19 +31,20 @@ public class NumberOfIsland_dfs {
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
 				if (grid[i][j] == '1') {
-					System.out.println("i " + i + " j " + j);
+					System.out.println("======================i " + i + " j " + j);
 					count++;
 					dfs(grid, i, j);
 				}
 			}
 		}
+		print(grid);
 		return count;
 	}
 
 	public void dfs(char[][] grid, int i, int j) {
 		dfsCount++;
 		System.out.println("===i " + i + " j " + j + " dfsCount " + dfsCount);
-
+		print(grid);
 		// 1.error
 //		if(i<0 || j<0 || i >= m || j >= n || grid[i][j]!='1') 
 //			return;
@@ -61,6 +62,18 @@ public class NumberOfIsland_dfs {
 //		dfs(grid, i+1,j);
 //		dfs(grid, i,j-1);
 //		dfs(grid, i,j+1);
+	}
+
+	public void print(char[][] grid) {
+		for (int i = 0; i < grid.length; i++) {
+//			System.out.print("i "+i);
+			for (int j = 0; j < grid[i].length; j++) {
+//				System.out.println(grid[i]);
+				System.out.print("[" + i + "][" + j + "]" + grid[i][j]);
+//				System.out.print(grid[i][j]);
+			}
+			System.out.println();
+		}
 	}
 
 }
