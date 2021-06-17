9bff89b75825a89fdb454945f791ed8d9f1b9905
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-06-17 22:16:19 +0900

    Update 03_LicenseKeyFormatting.java

--- String/03_LicenseKeyFormatting.java
+++ String/03_LicenseKeyFormatting.java
@@ -1,28 +1,42 @@
-package p1;
+package string;
 
 public class LicenseKeyFormatting {
-
+	
 	public static void main(String[] args) {
-
-		String S = "5F3Z-2e-9-w";
-//		String S = "2-5g-3-J";
-		int k = 4;
-		System.out.println(licenseKeyFormatting(S, k));
+		String str = "8F3Z-2e-9-wabcdef";
+//		String str = "8-5g-3-J";
+		int k =5;
+		solve(str, k);
 	}
-
-	public static String licenseKeyFormatting(String S, int K) {
-
-		String S1 = S.replace("-", "");
-		S1 = S1.toUpperCase();
-
+	
+	public static String solve(String str, int k ) {
+		String newStr = str.replace("-", "");
+		System.out.println("newStr: "+newStr);
+		
+		newStr = newStr.toUpperCase();
+		System.out.println("newStr: "+newStr);
+		
+		int leng = newStr.length();
+		System.out.println("leng: "+leng);
+		
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < S1.length(); i++) {
-			sb.append(S1.charAt(i));
+		for(int i=0; i<leng; i++) {
+			sb.append(newStr.charAt(i));
 		}
-		int len = sb.toString().length();
-		for (int i = K; i < len; i = i + K) {
-			sb.insert(len - i, '-');
+		System.out.println("sb: "+sb);
+		
+		//4,8,12
+		for(int i=k; i<leng; i=i+k) {
+		  System.out.println("leng-i: "+(leng-i));
+		  sb.insert(leng-i, '-');
 		}
-		return sb.toString();
+		System.out.println("--sb: "+sb);
+		
+		
+		return newStr;
+		
+		
+		
 	}
+
 }
