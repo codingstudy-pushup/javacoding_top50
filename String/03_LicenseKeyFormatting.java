f373c690e41c4b0dbac061bbf41306a62c42971f
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-06-17 23:20:36 +0900

    Update 03_LicenseKeyFormatting.java

--- String/03_LicenseKeyFormatting.java
+++ String/03_LicenseKeyFormatting.java
@@ -4,38 +4,33 @@ public class LicenseKeyFormatting {
 	
 	public static void main(String[] args) {
 		String str = "8F3Z-2e-9-wabcdef";
+//		String str = "8F3Z-2e-9-w";
 //		String str = "8-5g-3-J";
-		int k =5;
-		solve(str, k);
+		int k =4;
+		System.out.println(solve(str, k));
 	}
 	
 	public static String solve(String str, int k ) {
-		String newStr = str.replace("-", "");
-		System.out.println("newStr: "+newStr);
 		
-		newStr = newStr.toUpperCase();
-		System.out.println("newStr: "+newStr);
+		//1. - del
+		String newStr= str.replace("-", "");
+		System.out.println("1: "+newStr);
 		
-		int leng = newStr.length();
-		System.out.println("leng: "+leng);
+		//2. upper
+		newStr= newStr.toUpperCase();
+		System.out.println("2: "+newStr);
 		
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<leng; i++) {
-			sb.append(newStr.charAt(i));
+		//3. k개로 파싱하기, 그리고 - 넣기
+		StringBuilder sb = new StringBuilder(newStr);
+		System.out.println("3: "+newStr);
+//		"8F3Z2E9W"
+		int len = sb.length();
+		for(int i=k; i<len; i=i+k) {
+			System.out.println("len-i: "+(len-i));
+			sb.insert(len-i, '-');
+			System.out.println("4: "+sb);
 		}
-		System.out.println("sb: "+sb);
-		
-		//4,8,12
-		for(int i=k; i<leng; i=i+k) {
-		  System.out.println("leng-i: "+(leng-i));
-		  sb.insert(leng-i, '-');
-		}
-		System.out.println("--sb: "+sb);
-		
-		
-		return newStr;
-		
-		
+		return sb.toString();
 		
 	}
 
