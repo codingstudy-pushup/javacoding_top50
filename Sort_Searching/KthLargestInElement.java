8a575bb25d3d9e8c5b0bbaf329ab45619a8ced07
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-06-19 09:20:20 +0900

    Update KthLargestInElement.java

--- Sort_Searching/KthLargestInElement.java
+++ Sort_Searching/KthLargestInElement.java
@@ -6,29 +6,41 @@ public class KthLargestInElement {
 
 	public static void main(String[] args) {
 		KthLargestInElement a = new KthLargestInElement();
-//		int[] nums = { 3, 2, 1, 5, 6, 4 };
-		int[] nums = { 2, 3, 1, 5, 6, 4 };
-		int k = 2;
+//		int[] nums = { 2, 3, 1, 5, 6, 4 };
+//		int k = 2;
+		
+		int[] nums = { 3,2,3,1,2,4,5,5,6 };
+		int k = 4;
+		System.out.println(a.solve(nums, k));
 		System.out.println(a.solve_pq(nums, k));
 	}
 
 	public int solve(int[] nums, int k) {
-		final int N = nums.length;
 		Arrays.sort(nums);
-		return nums[N - k];
+		int n = nums.length;//6
+		return nums[n-k]; //6-2=4
+		
 	}
 
 	public int solve_pq(int[] nums, int k) {
-
+		
+		//1
 		Queue<Integer> pq = new PriorityQueue<>();
-
-		for (int val : nums) {
-			pq.offer(val);
-
-			if (pq.size() > k) {
+		
+		//2
+		for(int i: nums) {
+			pq.offer(i);
+			if(pq.size() >k) {
 				pq.poll();
 			}
 		}
 		return pq.peek();
+		
 	}
+	
+	
+	
+	
+	
+	
 }
