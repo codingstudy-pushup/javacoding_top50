2c21152185208b1d0d5f914902cf6f02f120c843
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-06-30 14:37:17 +0900

    Update FindAllAnagramInAString.java

--- TwoPointers/FindAllAnagramInAString.java
+++ TwoPointers/FindAllAnagramInAString.java
@@ -5,13 +5,49 @@ import java.util.*;
 public class FindAllAnagramInAString {
 	public static void main(String args[]) {
 		FindAllAnagramInAString a = new FindAllAnagramInAString();
-//		String src = "BACDGABCDA";
+//		String s = "BACDGABCDA";
 //		String trg = "ABCD";
-		String src = "bacdgabcda";
-		String tar = "abcd";
-		System.out.println(a.solve(src, tar));
+		String s = "bacdgabcda";
+		String p = "abcd";
+		System.out.println(a.solve_array(s, p));
 	}
-	
+
+	public List<Integer> solve_array(String s, String p) {
+		// 1
+		List<Integer> result = new ArrayList<>();
+		if (s == null || s.length() == 0 || p == null || p.length() == 0 || s.length() < p.length())
+			return result;
+		// 2
+		int[] pArr = new int[26];//
+		for (int i = 0; i < p.length(); i++) {
+			pArr[p.charAt(i) - 'a']++;// [1,1,1,1,0,0,0.......]
+		}
+
+		for (int i = 0; i < s.length() - p.length() + 1; i++) {
+			int[] sArr = new int[26];
+			for (int j = 0; j < p.length(); j++) {
+				System.out.print(s.charAt(i + j));
+				System.out.println("i " + i + " j " + j);
+				sArr[s.charAt(i + j) - 'a']++; // 0+, 1+
+
+			}
+			System.out.println();
+			if (check(pArr, sArr)) {
+				result.add(i);
+			}
+		}
+		return result;
+	}
+
+	private boolean check(int[] pArr, int[] sArr) {
+		for (int i = 0; i < pArr.length; i++) {
+			if (pArr[i] != sArr[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public List<Integer> solve(String s, String t) {
 		List<Integer> result = new LinkedList<>();
 		if (t.length() > s.length())
@@ -40,56 +76,19 @@ public class FindAllAnagramInAString {
 					map.put(c2, map.get(c2) + 1);
 					if (map.get(c2) > 0) {
 						counter++;
-						System.out.println("cou "+counter);
+						System.out.println("cou " + counter);
 					}
 				}
 				if (right - left == t.length()) {
 					result.add(left);
 				}
-			
+
 				left++;
-				
+
 			}
 
 		}
 		return result;
 	}
-
-	public List<Integer> solve_array(String src, String tar) {
-		// 1
-		List<Integer> result = new ArrayList<>();
-		if (src == null || src.length() == 0 || tar == null || tar.length() == 0 || src.length() < tar.length())
-			return result;
-		// 2
-		int[] tarArr = new int[26];//
-		for (int i = 0; i < tar.length(); i++) {
-			tarArr[tar.charAt(i) - 'a']++;// [1,1,1,1,0,0,0.......]
-		}
-
-		for (int i = 0; i < src.length() - tar.length() + 1; i++) {
-			int[] srcArr = new int[26];
-			for (int j = 0; j < tar.length(); j++) {
-				System.out.print(src.charAt(i + j));
-				System.out.println("i " + i + " j " + j);
-				srcArr[src.charAt(i + j) - 'a']++; // 0+, 1+
-
-			}
-			System.out.println();
-			if (check(tarArr, srcArr)) {
-				result.add(i);
-			}
-		}
-		return result;
-	}
-
-	private boolean check(int[] tarArr, int[] srcArr) {
-		for (int i = 0; i < tarArr.length; i++) {
-			if (tarArr[i] != srcArr[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
-
 
 }
