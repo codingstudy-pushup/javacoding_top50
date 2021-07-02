4477eb5bc138379bf44fe6056f2016dc6960914f
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-07-02 16:34:36 +0900

    Update 02_ValidParentheses.java

--- Queue&Stack/02_ValidParentheses.java
+++ Queue&Stack/02_ValidParentheses.java
@@ -11,7 +11,23 @@ public class ValidParentheses {
 		System.out.println(solve(exp));
 	}
 	
-	
+      public boolean solve_1(String s) {
+        Stack<Character> stack = new Stack<>();
+    
+        for(int i = 0; i<s.length(); i++) {
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+                stack.push(s.charAt(i));
+            else if(s.charAt(i) == ')' && !stack.empty() && stack.peek() == '(')
+                stack.pop();
+            else if(s.charAt(i) == ']' && !stack.empty() && stack.peek() == '[')
+                stack.pop();
+            else if(s.charAt(i) == '}' && !stack.empty() && stack.peek() == '{')
+                stack.pop();
+            else
+                return false;
+        }
+        return stack.empty();
+    }
 	public static boolean solve(String str) {
 		Stack<Character> stack = new Stack<>();
 		for(char c: str.toCharArray()) {
